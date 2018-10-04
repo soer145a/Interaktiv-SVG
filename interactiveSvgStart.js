64aa802d258e2904684e7b51b45924dbc5510c7a
martinbregnhoi <mabe@kea.dk>
2018-10-04 10:42:29 +0200

    starter files added

---
+++ interactiveSvgStart.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", function () {
+    runProgram();
+});
+
+async function runProgram() {
+    
+   
+
+    // 1. Load svg map
+    //------------------------------------------------------------------------------------	
+ 
+
+    // 2. find infobokse og skjul dem
+    //------------------------------------------------------------------------------
+
+
+    // 3. Skift farve ved klik, og vis tekst
+    //-----------------------------------------------------------------------
+
+
+    //function clicked
+    //--------------------------------------------------------------------
+
+    
+    // a. find det klikkede element
+    //----------------------------------------------
+ 
+    
+    // b. find det klikkede elementets ID
+    //---------------------------------------------
+
+  
+    
+    // c. find  det klikkede elements fillfarve
+    //---------------------------------------------
+
+ 
+    // d. vis infobokse
+    //--------------------------------------------
+
+  
+
+    // 4. hvis der tidligere har været klikket skal det forige element skifte farve til original
+    //------------------------------------------------------------------------------------
+  
+    //gør det klikkede til det aktive
+    //-------------------------------------------------------------------------
+
+
+    //skift farve på det valgte
+    //-------------------------------------------------------------------------
+
+    //reset farve og skjul tekst hvis valgt elementet allerede er aktivt
+    //--------------------------------------------------------------------------
+
+
+};
